05b10f9206922387305d096831d22ca365b0d1c5
msjie <ligongdamao@163.com>
2019-12-17 16:49:33 +0800

    init

---
+++ src/main/java/cn/yusite/hello/maven/release/module/a/ModuleAApplication.java
@@ -0,0 +1,13 @@
+package cn.yusite.hello.maven.release.module.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/yusite/hello/maven/release/module/a/ModuleAApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yusite.hello.maven.release.module.a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
